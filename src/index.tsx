500beb5fd5f72df8dee4a7652a49444570a45236
Caleb Taylor <aquaductape@gmail.com>
2021-09-10 01:58:54 -0700

    update

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "solid-js/web";
-
-import "simplebar";
-import "simplebar/dist/simplebar.css";
-import "focus-visible";
-import "./index.css";
-import App from "./App";
-
-render(() => <App />, document.getElementById("root")!);
