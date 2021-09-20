3a547575c73ef5c6ef7459229d2047b09383e6e2
Caleb Taylor <aquaductape@gmail.com>
2021-09-20 01:29:55 -0700

    update

--- test/src/components/IFrame.tsx
+++ test/src/components/IFrame.tsx
@@ -27,7 +27,15 @@ const IFrame: Component<{
       doc.close();
     });
   });
-  return <iframe class={"iframe"} src="" ref={el}></iframe>;
+  return (
+    <iframe
+      class={"iframe"}
+      src=""
+      // @ts-ignore
+      scrolling="no"
+      ref={el}
+    ></iframe>
+  );
 };
 
 export default IFrame;
