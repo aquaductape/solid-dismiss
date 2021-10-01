6e5421db0a513d98d422d45394ca947163c0f7e2
Caleb Taylor <aquaductape@gmail.com>
2021-10-01 14:31:55 -0700

    update

--- test/src/index.tsx
+++ test/src/index.tsx
@@ -1,9 +1,5 @@
 import { render } from "solid-js/web";
 
-//@ts-ignore
-import smoothscroll from "smoothscroll-polyfill";
-smoothscroll.polyfill();
-
 import "./index.css";
 import App from "./App";
 
