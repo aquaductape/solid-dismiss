ffe54f61ba24dc68e8d299f0ae5a5a5483f72bc6
Caleb Taylor <aquaductape@gmail.com>
2021-10-01 14:22:33 -0700

    update

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -3,7 +3,10 @@ import solidPlugin from "vite-plugin-solid";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [solidPlugin(), viteSingleFile()],
+  plugins: [
+    solidPlugin(),
+    //  viteSingleFile()
+  ],
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
