93c4d9cdfeb24f0694ef04b81b4d5982732b13a4
Caleb Taylor <aquaductape@gmail.com>
2021-10-01 14:19:50 -0700

    update

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -6,17 +6,17 @@ export default defineConfig({
   plugins: [solidPlugin(), viteSingleFile()],
   build: {
     target: "esnext",
-    // polyfillDynamicImport: false,
-    assetsInlineLimit: 100000000,
-    chunkSizeWarningLimit: 100000000,
-    cssCodeSplit: false,
-    brotliSize: false,
-    rollupOptions: {
-      inlineDynamicImports: true,
-      output: {
-        manualChunks: () => "everything.js",
-      },
-    },
+    polyfillDynamicImport: false,
+    // assetsInlineLimit: 100000000,
+    // chunkSizeWarningLimit: 100000000,
+    // cssCodeSplit: false,
+    // brotliSize: false,
+    // rollupOptions: {
+    //   inlineDynamicImports: true,
+    //   output: {
+    //     manualChunks: () => "everything.js",
+    //   },
+    // },
   },
   server: {
     fs: {
